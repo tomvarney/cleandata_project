dc7f22c41b899b48559b435a210953fcd729e5f9
tomvarney <tvarney@tvarney.com>
2015-03-16 09:49:17 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+
+To get the tidy data for the first dataset ("observations.csv") I did the folowing:
+
+1. Load the X, y, and subject data (Training and Test) into a totla of 6 data.frames
+	I assumed that the files are in the working directory, but...since not sure if training and test files will be directly in working directory, or unzipped into folders, I test for existence and try both at the root level of the unzipped folder, as well as in the separate /train and /test folders for all 3 sets of files (X, y, subjects)
+
+	X_train.txt
+  X_test.txt
+  y_train.txt
+  y_test.txt
+  subject_train.txt
+  subject_test.txt
+
+2. I then combine the training and test sets for each of X, y, subjects using rbind -- so now have 3 data.frames, one for X, y, and subject. 
+
+3. I loaded the feature labels from features.txt
+
+4. I used grep to get only those features that had "mean()" or "std()" in them. I needed to use the Parentheses in teh regular expression because I decided to exclude the columns that were not direct means of the measurements -- like "...meanFreq".  I then transformed this data.frame into a character vector, and also saved a vector of the indices of the desired columns in order to use below to select these cols form the dataset.
+
+5. I cleaned up the variable names to remove the parens and replace the dashes with underscores (using gsub).
+
+6. I used the resulting feature index vector to select only the desired cols from the dataset
+
+7. I used the cleaned up list of features to apply as the names to the variables
+
+8. I added the subject and activity (y) columns to the dataset as factor variables named "subject" and "activity"
+
+9. I created descriptive character labels for the activity factor:  c(
+	X_train$activity <- factor(X_train$activity, labels = c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING"))
+
+10. I wrote out the resulting tidy dataset of observations to a file "observations.csv"
+
+###
+PART 2 -- Create a summary dataset of averages of reading for all subject-activity pairs.
+
+1. I loaded the data.table library and loaded the data.frame from above into it.  This should allow it to be processed more efficiently.
+
+2. I computed the averages using lapply inside the data.table command using code: 
+		df_averages <- DT[, lapply(.SD,mean), by = "subject,activity"]
+3. I converted the data.table back into a data.frame
+
+4. I created the names for the variables in the "averages" dataset by appending "_avg" to all of the variable names using the paste command:
+	features_avg <- paste(features, "_avg", sep="")
+	I also added 2 names for the grouping variables "subject" and "activity"
+
+5. I wrote out the dataset into a file "subject_activity_averages.csv" using the write.table command:
+	write.table(df_averages, file = "subject_activity_averages.csv", sep = ",", row.names=FALSE, col.names = TRUE)
+
+
+
+
+
+
+
+
+
---
+++ run_analysis.R
@@ -0,0 +1,131 @@
+
+#setwd("~/R/rdata/UCI HAR Dataset")
+
+#get the X data Training and Test
+
+#ASSUMES that the files are in the working directory, but...
+#not sure if training and test files will be directly in working directory, or unzipped into folders
+# so try each one to see which ....
+if (file.exists("X_train.txt"))
+  {
+    X_train <- read.table("X_train.txt", quote="\"")
+  } else
+  {
+    X_train <- read.table("train/X_train.txt", quote="\"")
+  }
+
+if (file.exists("X_test.txt"))
+{
+  X_test <- read.table("X_test.txt", quote="\"")
+} else
+{
+  X_test <- read.table("test/X_test.txt", quote="\"")
+}
+
+         
+#get the labels Training and Test
+if (file.exists("y_train.txt"))
+{
+  y_train <- read.table("y_train.txt", quote="\"")
+} else
+{
+  y_train <- read.table("train/y_train.txt", quote="\"")
+}
+
+if (file.exists("y_test.txt"))
+{
+  y_test <- read.table("y_test.txt", quote="\"")
+} else
+{
+  y_test <- read.table("test/y_test.txt", quote="\"")
+}
+
+
+#get the subjects Training and Test
+if (file.exists("subject_train.txt"))
+{
+  subject_train <- read.table("subject_train.txt", quote="\"")
+} else
+{
+  subject_train <- read.table("train/subject_train.txt", quote="\"")
+}
+
+if (file.exists("subject_test.txt"))
+{
+  subject_test <- read.table("subject_test.txt", quote="\"")
+} else
+{
+  subject_test <- read.table("test/subject_test.txt", quote="\"")
+}
+
+
+#combine the training and test sets for each of X, y, subjects
+X_train <- rbind(X_train, X_test)
+y_train <- rbind(y_train, y_test)
+subject_train <- rbind(subject_train, subject_test)
+#######
+
+#get the feature labels
+features <- read.table("~/R/rdata/UCI HAR Dataset/features.txt", quote="\"")
+
+#find only the columns that end in "mean()" or "std()"  
+#(I decided to exclude the ones that were not direct means of the measurement -- like "...meanFreq")
+featuresToKeep <- grep("(mean\\()|(std\\()", features$V2)
+features <- features[featuresToKeep,][2]
+features <- as.character(features$V2)
+
+#clean up the variable names to remove the parens and replace the dashes with underscores
+features <- gsub("\\(|\\)","",features)
+features <- gsub("-","_",features)
+
+#select only the desired cols from the dataset
+X_train <- X_train[featuresToKeep]
+
+
+#apply the names to the variables
+names(X_train) <- features
+X_train$subject <- as.factor(subject_train[[1]])
+X_train$activity <- as.factor(y_train[[1]])
+
+#create charater labels for the activity factor
+X_train$activity <- factor(X_train$activity, labels = c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", "SITTING", "STANDING", "LAYING"))
+
+#check the resulting structure
+str(X_train)
+
+#write out the finished dataset
+write.table(X_train, file = "observations.csv", sep = ",", row.names=FALSE, col.names = TRUE)
+
+######################################################
+#create data set of averages by subject and activity
+
+#create data.table for speed/efficiency
+library(data.table)
+DT <- data.table(X_train)
+
+#compute the averages using lapply inside the data.table command
+df_averages <- DT[, lapply(.SD,mean), by = "subject,activity"]
+
+#convert back to dataframe
+df_averages <- as.data.frame(df_averages)
+
+#get the names for the avg dataset -- append "_avg" to all of the variable names
+features_avg <- paste(features, "_avg", sep="")
+features_avg <- c("subject", "activity", features_avg)
+names(df_averages) <- features_avg
+
+#take a look at the new dataset
+head(df_averages)
+str(df_averages)
+
+#write it out
+write.table(df_averages, file = "subject_activity_averages.csv", sep = ",", row.names=FALSE, col.names = TRUE)
+
+
+
+
+
+
+
+
+
